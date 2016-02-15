0460f6b5aef54e6717d230fc8d72d52b516e586e
theuerse <theuers@posteo.de>
2016-02-15 09:31:09 +0100

    tried to make graph options global instead of individual per-Pi

--- stat.php
+++ stat.php
@@ -364,58 +364,7 @@ function genGraph($pi_index, $dtype, $datasets)
     // insert a labeled canvas as container for the chart
     echo "<strong>".$dtype." [".$SI."]</strong>:<br><canvas id='$dtype-$pi_index' width='1000' height='400'></canvas>
     <script>
-      // define options for the graph
-      var options_".$dtype."_".$pi_index." = {
-
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines : true,
-
-        //String - Colour of the grid lines
-        scaleGridLineColor : 'rgba(0,0,0,.05)',
-
-        //Number - Width of the grid lines
-        scaleGridLineWidth : 1,
-
-        // Set the start value
-        scaleBeginAtZero: true,
-
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-
-        //Boolean - Whether the line is curved between points
-        bezierCurve : false,
-
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension : 0.4,
-
-        //Boolean - Whether to show a dot for each point
-        pointDot : true,
-
-        pointDotRadius : 4,
-
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth : 1,
-
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius : 20,
-
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke : true,
-
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth : 2,
-
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill : true,
-
-        //String - A legend template
-        legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-      };
-
-      // specify the actual data
+      // specify the actual data for the individual Pi
       var data_".$dtype."_".$pi_index." = {
         labels: [$labels],
         datasets: [
@@ -432,7 +381,7 @@ function genGraph($pi_index, $dtype, $datasets)
         ]
       };
       // draw chart in canvas
-      var $dtype".$pi_index."_chart=new Chart(document.getElementById('$dtype-$pi_index').getContext('2d')).Line(data_".$dtype."_".$pi_index.", options_".$dtype."_".$pi_index.");
+      var $dtype".$pi_index."_chart=new Chart(document.getElementById('$dtype-$pi_index').getContext('2d')).Line(data_".$dtype."_".$pi_index.", graphOptions);
     </script>";
 }
 
@@ -489,6 +438,44 @@ if ($_GET["action"] == "save" && $_GET["key"] == "$historykey") {
               return false;
           });
       });
+
+      // define global options for all history-graphs
+      var graphOptions = {
+
+          scaleShowGridLines : true, //Boolean - Whether grid lines are shown across the chart
+
+          scaleGridLineColor : 'rgba(0,0,0,.05)', //String - Colour of the grid lines
+
+          scaleGridLineWidth : 1, //Number - Width of the grid lines
+
+          scaleBeginAtZero: true, // Set the start value
+
+          scaleShowHorizontalLines: true, //Boolean - Whether to show horizontal lines (except X axis)
+
+          scaleShowVerticalLines: true, //Boolean - Whether to show vertical lines (except Y axis)
+
+          bezierCurve : false, //Boolean - Whether the line is curved between points
+
+          bezierCurveTension : 0.4, //Number - Tension of the bezier curve between points
+
+          pointDot : true, //Boolean - Whether to show a dot for each point
+
+          pointDotRadius : 4,
+
+          pointDotStrokeWidth : 1, //Number - Pixel width of point dot stroke
+
+          //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+          pointHitDetectionRadius : 20,
+
+          datasetStroke : true, //Boolean - Whether to show a stroke for datasets
+
+          datasetStrokeWidth : 2, //Number - Pixel width of dataset stroke
+
+          datasetFill : true, //Boolean - Whether to fill the dataset with a colour
+
+          //String - A legend template
+          //legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+      };
     </script>
 
     <style type="text/css">
